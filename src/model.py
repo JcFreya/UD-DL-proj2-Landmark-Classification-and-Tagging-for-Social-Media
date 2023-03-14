cb8784e0149622231b70d74bc35c3a59615d2847
Fan Yuan <yfan941101@gmail.com>
2023-03-14 02:40:48 -0400

    Add model script

--- src/model.py
+++ src/model.py
@@ -13,14 +13,67 @@ class MyModel(nn.Module):
         # to size appropriately the output of your classifier, and if you use
         # the Dropout layer, use the variable "dropout" to indicate how much
         # to use (like nn.Dropout(p=dropout))
+        self.model = nn.Sequential(
+            
+            # first conv layer + batchnorm + relu + maxpool
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1), #224x224x3 -> 224x224x16 
+            nn.BatchNorm2d(16),
+            nn.MaxPool2d(2, 2),# -> maxpool 112x112x16
+            nn.ReLU(),
+            nn.Dropout2d(dropout),
+            
+            # second conv layer + batchnorm + relu + maxpool
+            nn.Conv2d(16, 32, 3, padding=1),  #112x112x16 -> 112x112x32
+            # Add batch normalization (BatchNorm2d) here
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2, 2),  # -> maxpool 56x56x32
+            nn.ReLU(),
+            nn.Dropout2d(dropout),
+            
+            # third conv layer + batchnorm + relu + maxpool
+            nn.Conv2d(32, 64, 3, padding=1),  # 56x56x32 -> 56x56x64 
+            # Add batch normalization (BatchNorm2d) here
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),  # -> maxpool 28x28x64
+            nn.ReLU(),
+            nn.Dropout2d(dropout),
+            
+            # Since we are using BatchNorm and data augmentation,
+            # we can go deeper than before and add one more conv layer
+            # forth conv layer + batchnorm + relu + maxpool
+            nn.Conv2d(64, 128, 3, padding=1),  # 28x28x64 -> 28x28x128 
+            # Add batch normalization (BatchNorm2d) here
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, 2),  # -> maxpool 14x14x128
+            nn.ReLU(),
+            nn.Dropout2d(dropout),
+            
+            # fifth conv layer + batchnorm + relu + maxpool
+            nn.Conv2d(128, 256, 3, padding=1),  # 14x14x128 -> 14x14x256
+            # Add batch normalization (BatchNorm2d) here
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # -> maxpool 7x7x256
+            nn.Dropout2d(dropout),
+            
+            nn.Flatten(),  # -> 1x7x7x256
+            
+            nn.Linear(7 * 7 * 256, 512),  # -> 512
+            nn.Linear(512, 128),
+            nn.Dropout(0.5),
+            # Add batch normalization (BatchNorm1d, NOT BatchNorm2d) here
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # YOUR CODE HERE: process the input tensor through the
         # feature extractor, the pooling and the final linear
         # layers (if appropriate for the architecture chosen)
+        x = self.model(x)
         return x
-
-
+    
 ######################################################################################
 #                                     TESTS
 ######################################################################################
